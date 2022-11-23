4a2334c00a8d9d9d93e2be9ac6aa44fc46e87692
inix84 <inix84@mail.ru>
2022-11-24 02:26:15 +0500

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
         human Anya = new human("жен","Аня","Москва",1993,"методист образовательных программ");
         human Katya = new human("жен","Катя","Калининград",1992,"продакт-менеджер");
         human Artem = new human("муж","Артем","Москва",1995,"директор по развитию бизнеса");
+        human spy = new human("муж","","",-1995,"");
         System.out.println(Maxim);
         System.out.println(Anya);
         System.out.println(Katya);
         System.out.println(Artem);
+        System.out.println(spy);
     }
 }
--- src/human.java
+++ src/human.java
@@ -7,10 +7,26 @@ public class human {
 
     human(String gender, String name,String town, int yearOfBirth, String jobTitle) {
         this.gender = gender;
-        this.name=name;
-        this.town = town;
-        this.yearOfBirth = yearOfBirth;
-        this.jobTitle = jobTitle;
+        if (yearOfBirth>0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
+        if (town!=null && town.length()!=0) {
+            this.town = town;
+        } else {
+            this.town = "<<Информация не указана>>";
+        }
+        if (name!=null && name.length()!=0) {
+            this.name=name;
+        } else {
+            this.name = "<<Информация не указана>>";
+        }
+        if (jobTitle!=null && jobTitle.length()!=0) {
+            this.jobTitle = jobTitle;
+        } else {
+            this.jobTitle = "<<Информация не указана>>";
+        }
     }
     @Override
     public String toString() {
